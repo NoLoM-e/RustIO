b4132d33234102daf4a9dcd81c6ecd09128ae811
NoLoM-e <dyjey18@gmail.com>
2024-05-23 17:36:52 +0300

    added source code itself...

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        panic!("File name was not provided as run argument");
+    }
+
+    let fileToRead = File::open(&args[1]);
+    let file = match fileToRead {
+        Ok(file) => file,
+        Err(error) => panic!("An Error accured {:?}", error),
+    };
+
+    let reader = BufReader::new(file);
+    let mut lineCounter = 1;
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                println!("Line {} from file {}", lineCounter, line);
+                lineCounter += 1;
+            }
+            Err(error) => {
+                panic!(
+                    "An error {} accured while reading line number {}",
+                    error, lineCounter
+                );
+            }
+        };
+    }
+}
