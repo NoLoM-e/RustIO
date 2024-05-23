4f41219226256316f9eed71a55913eca81da96fc
NoLoM-e <dyjey18@gmail.com>
2024-05-23 17:39:32 +0300

    changed variable to snake case

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,24 @@ fn main() {
         panic!("File name was not provided as run argument");
     }
 
-    let fileToRead = File::open(&args[1]);
-    let file = match fileToRead {
+    let file_to_read = File::open(&args[1]);
+    let file = match file_to_read {
         Ok(file) => file,
         Err(error) => panic!("An Error accured {:?}", error),
     };
 
     let reader = BufReader::new(file);
-    let mut lineCounter = 1;
+    let mut line_counter = 1;
     for line in reader.lines() {
         match line {
             Ok(line) => {
-                println!("Line {} from file {}", lineCounter, line);
-                lineCounter += 1;
+                println!("Line {} from file {}", line_counter, line);
+                line_counter += 1;
             }
             Err(error) => {
                 panic!(
                     "An error {} accured while reading line number {}",
-                    error, lineCounter
+                    error, line_counter
                 );
             }
         };
